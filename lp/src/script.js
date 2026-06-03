876fe713388b533a1a488c079427d1d41c0ff99e
thiagohannemann7 <thiagohannemann7@gmail.com>
2026-06-03 16:56:19 -0300

    Add files via upload

--- lp/src/script.js
+++ lp/src/script.js
@@ -1,6 +1,14 @@
-function navTo(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
+function navTo(id){
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
 }
+
+function enviar(){
+    const nome = document.getElementById('nome').value 
+    const assunto = document.getElementById('assunto').value 
+    const mensagem = `Gostaria de entrar em contato!\n\nNome: ${nome} \nAssunto: ${assunto}`   
+    const msg = encodeURIComponent(mensagem)
+    window.open(`https://wa.me/5541*********?text=${msg}`)
+} 
